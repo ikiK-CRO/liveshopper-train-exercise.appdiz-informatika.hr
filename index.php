ac9bf58b8a71c84e3b00556b9de3c859210108e2
ikiK <kiki.ikik@gmail.com>
2021-06-24 16:58:08 +0200

    -a

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php 
+<?php
 
+class Train
+{
+    public $TrainCars = [];
+}
+
+class TrainCar
+{
+    public $weight;
+
+    function set_weight($weight)
+    {
+        $this->weight = $weight;
+    }
+    function get_weight()
+    {
+        return $this->weight;
+    }
+}
+
+
+$car1 = new TrainCar();
+$car1->set_weight('1t');
+
+$car2 = new TrainCar();
+$car2->set_weight('2t');
+
+echo $car1->get_weight()."<br>";
+echo $car2->get_weight()."<br>";
