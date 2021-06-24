10d29bc718f15516650ce3f25ec405be552a6df5
ikiK <kiki.ikik@gmail.com>
2021-06-24 17:15:43 +0200

    -a

--- index.php
+++ index.php
@@ -3,7 +3,23 @@
 class Train
 {
     public $TrainCars = [];
+
+    function set_TrainCars($car, $position)
+    {
+        if ($position === "back") {
+            array_push($this->TrainCars, $car);
+        } elseif ($position === "front") {
+            array_unshift($this->TrainCars, $car);
+        }
+    }
+
+    function get_Train()
+    {
+        return $this;
+    }
 }
+
+$train = new Train();
 
 class TrainCar
 {
@@ -26,5 +42,11 @@ $car1->set_weight('1t');
 $car2 = new TrainCar();
 $car2->set_weight('2t');
 
-echo $car1->get_weight()."<br>";
-echo $car2->get_weight()."<br>";
+// echo $car1->get_weight() . "<br>";
+// echo $car2->get_weight() . "<br>";
+
+$train->set_TrainCars($car1, "front");
+$train->set_TrainCars($car2, "back");
+
+// echo $train->get_Train() . "<br>";
+print_r($train->get_Train());
