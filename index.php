db8071536922398cafbee0136fc3c90c8ba6f6fd
ikiK <kiki.ikik@gmail.com>
2021-06-26 00:30:31 +0200

    -a

--- index.php
+++ index.php
@@ -8,6 +8,31 @@ ini_set('error_reporting', E_ALL);
 class Train
 {
     public $TrainCars = [];
+    public $carTypes;
+    public $carPositions;
+
+    public function __construct()
+    {
+        $this->carTypes = ["cargo", "passenger", "engine"];
+        $this->carPositions = ["back", "front"];
+    }
+
+    public function addCars($car)
+    {
+
+        if (sizeof($car) === 3 && is_numeric($car[0]) &&  in_array($car[1], $this->carTypes) &&  in_array($car[1], $this->carTypes) &&  in_array($car[2], $this->carPositions)) {
+            $newcar = new TrainCar();
+            $newcar->set_weight($car[0]);
+            $newcar->set_type($car[1]);
+
+            if ($this->set_TrainCars($newcar, $car[2]) === false) {
+                return  "Limit of cars excited<br>";
+            }
+        } else {
+            return  "Invalid car entry value<br>";
+        }
+    }
+
 
     function set_TrainCars($car, $position) // TAKES TrainCar OBJECT AND POSITION ON TRAN AS ARGUMENTS
     {
@@ -24,14 +49,18 @@ class Train
 
     function remove_TrainCars($position) // TAKES POSITION OF CAR AS ARGUMENT
     {
-        if (sizeof($this->TrainCars) != 0) {
-            if ($position === "front") {
-                array_shift($this->TrainCars);
-            } elseif ("back") {
-                array_pop($this->TrainCars);
+        if (in_array($position, $this->carPositions)) {
+            if (sizeof($this->TrainCars) != 0) {
+                if ($position === "front") {
+                    array_shift($this->TrainCars);
+                } elseif ("back") {
+                    array_pop($this->TrainCars);
+                }
+            } else {
+                return "No cars to remove from train<br>";
             }
         } else {
-            return false;
+            return "Position can be only front or back<br>";
         }
     }
 
@@ -84,56 +113,23 @@ class TrainCar
 // NEW TRAIN
 $train = new Train();
 
-//sanitation pre defined arrays
-$carTypes = ["cargo","passenger","engine"];
-$carPositions = ["back", "front"];
-
-
-
-
-
 
 //EXAMPLES:
 
 //ARRAY ADDING USING FUNCTION
-$cars = [[1, "cargo", "front"], [2.4, "passenger", "back"], [3.6, "passenger", "front"]];
+$cars = [[1, "cargo", "front"], [2.4, "passenger", "back"], [3.6, "cargo", "front"]];
 
 foreach ($cars as $car) {
-    addCars($car, $train, $carTypes, $carPositions);
+    print_r($train->addCars($car));
 }
 
 
 //NEW SINGLE CAR USING FUNCTION
 $newcar = ["2.5", "cargo", "front"];
-addCars($newcar, $train, $carTypes, $carPositions);
-
-
-// CREATE, FILL AND ADD CARS TO TRAIN FUNCTION
-function addCars($car, $train, $carTypes, $carPositions)
-{
-
-    if (sizeof($car) === 3 && is_numeric($car[0]) &&  in_array($car[1], $carTypes) &&  in_array($car[1], $carTypes) &&  in_array($car[2], $carPositions)) {
-        $newcar = new TrainCar();
-        $newcar->set_weight($car[0]);
-        $newcar->set_type($car[1]);
-    
-        if ($train->set_TrainCars($newcar, $car[2]) === false) {
-            echo  "Limit of cars excited<br>";
-        }
-
-    }else{
-        echo  "Invalid car entry array<br>";
-    }
-
-}
-
+print_r($train->addCars($newcar));
 
 //REMOVE CAR with:  "back" OR "front" ie: pre-defined $carPositions index array
-if ($train->remove_TrainCars($carPositions[1]) === false) {
-    echo  "No cars to remove from train<br>";
-}
-
-
+print_r($train->remove_TrainCars("front"));
 
 
 
@@ -148,19 +144,3 @@ echo  $train->get_Train_Carts()[0]->get_weight() . " - particular train car weig
 
 // GET TYPE OF PARTICULAR TRAIN CAR USING INDEX OF CAR ON TRAIN
 echo  $train->get_Train_Carts()[0]->get_type() . " - particular train car type <br>";
-
-
-// print_r($train->get_Train_Carts()) . "<br>"; // GET ALL CARS IN TRAIN AS ARRAY
-
-
-
-//EXAMPLE WITHOUT ARRAYS FOR SINGLE CAR AND SAVING OBJECT
-$car1 = new TrainCar();
-$car1->set_weight('3.5');
-$car1->set_type('passenger');
-
-if ($train->set_TrainCars($car1, "back") === false) {
-    echo  "Limit of cars excited<br>";
-}
-//GET SINGLE CAR WEIGHT
-$car1->get_weight();
